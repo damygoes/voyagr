8f3b5a34476446af491bbceeef758407a0eb2277
Damilola Bada <badadamilola@gmail.com>
2025-05-28 23:13:08 +0200

    feat: refined auth layout and landping page with background image and adjusted styling

--- apps/web/src/features/authentication/components/AuthLayout.tsx
+++ apps/web/src/features/authentication/components/AuthLayout.tsx
@@ -17,26 +17,31 @@ export function AuthLayout({
   secondaryLinkHref: string;
 }) {
   return (
-    <main className="flex flex-col md:flex-row items-center justify-between h-screen w-full px-lg py-2xl md:px-xl bg-background">
+    <main className="flex flex-col md:flex-row h-screen w-full bg-background">
+      {/* Left Section with Background Image */}
       <section
-        className="flex-1 flex flex-col justify-center items-center gap-sm text-center max-w-lg mx-auto md:mx-0"
+        className="relative flex-1 flex items-center justify-center px-lg py-2xl md:px-xl text-center bg-cover bg-center bg-no-repeat text-white"
+        style={{ backgroundImage: "url('/images/auth-bg.jpg')" }}
         aria-labelledby="auth-title"
       >
-        <h1 id="auth-title" className="text-3xl font-bold">
-          {title}
-        </h1>
-        <p className="text-muted-foreground mt-sm" role="doc-subtitle">
-          {description}
-        </p>
-
-        <div className="mt-md">
-          <Button variant="link">
-            <Link href={secondaryLinkHref}>{secondaryLinkText}</Link>
-          </Button>
+        <div className="absolute inset-[0px] bg-[#000] opacity-50 z-0" />
+        <div className="relative z-10 max-w-lg text-white space-y-sm mb-3xl pb-3xl">
+          <h1 id="auth-title" className="text-3xl font-bold">
+            {title}
+          </h1>
+          <p className="text-white opacity-60 mt-sm" role="doc-subtitle">
+            {description}
+          </p>
+          <div className="mt-md">
+            <Button variant="ghost" size="sm">
+              <Link href={secondaryLinkHref}>{secondaryLinkText}</Link>
+            </Button>
+          </div>
         </div>
       </section>
 
-      <div className="w-full md:w-2/5 h-auto md:h-full flex justify-center items-center mt-xl md:mt-0 px-md">
+      {/* Right Section with Auth Form */}
+      <div className="w-full md:w-2/5 h-auto md:h-full flex justify-center items-center px-md">
         {children}
       </div>
     </main>
