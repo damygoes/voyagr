7a3023b0db45e008514623c12f6109bb86e376d8
Damilola Bada <badadamilola@gmail.com>
2025-05-28 13:00:41 +0200

    chore: modified package.json and vite-config in the packages/ui

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
@@ -7,7 +8,6 @@ export default defineConfig({
     react(),
     svgr({
       svgrOptions: {
-        // SVGR options here
         icon: true,
         svgo: true,
         exportType: "named",
@@ -16,11 +16,19 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: "./src/index.ts",
-      formats: ["es"],
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "VoyagrUI",
+      formats: ["es", "cjs"], // <-- output both ESM and CJS
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
