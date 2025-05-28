3596b7524e59db71a226d779669bc5e27f7ed258
Damilola Bada <badadamilola@gmail.com>
2025-05-28 23:18:07 +0200

    chore: added border radius to background image on auth page

--- apps/web/src/features/authentication/components/AuthLayout.tsx
+++ apps/web/src/features/authentication/components/AuthLayout.tsx
@@ -17,14 +17,14 @@ export function AuthLayout({
   secondaryLinkHref: string;
 }) {
   return (
-    <main className="flex flex-col md:flex-row h-screen w-full bg-background">
+    <main className="flex flex-col md:flex-row h-screen w-full bg-background py-md">
       {/* Left Section with Background Image */}
       <section
-        className="relative flex-1 flex items-center justify-center px-lg py-2xl md:px-xl text-center bg-cover bg-center bg-no-repeat text-white"
+        className="relative flex-1 flex items-center justify-center px-lg py-2xl md:px-xl text-center bg-cover bg-center bg-no-repeat text-white rounded-r-full"
         style={{ backgroundImage: "url('/images/auth-bg.jpg')" }}
         aria-labelledby="auth-title"
       >
-        <div className="absolute inset-[0px] bg-[#000] opacity-50 z-0" />
+        <div className="absolute inset-[0px] bg-[#000] opacity-50 z-0 rounded-r-full" />
         <div className="relative z-10 max-w-lg text-white space-y-sm mb-3xl pb-3xl">
           <h1 id="auth-title" className="text-3xl font-bold">
             {title}
